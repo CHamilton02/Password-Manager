6eab95ce0a52d574de3b49d910c05dc888d5f153
Camdyn Hamilton <c02hamil@gmail.com>
2024-05-10 15:51:20 -0400

    Upload main file.

---
+++ main.py
@@ -0,0 +1,17 @@
+password = input("Please enter your master password: ")
+userChoice = input("Password Manager Menu\n1 - View passwords\n2 - Add new password\n3 - Generate new password\n4 - Re-enter master password (if you made a misinput)\nQ - End program\nYour input: ")
+
+while userChoice.lower() != 'q':
+  if userChoice == '1':
+    print('Passwords!!!')
+  elif userChoice == '2':
+    print('New password!')
+  elif userChoice == '3':
+    print('12gdrg54536435')
+  elif userChoice == '4':
+    print('You misclicked lollll')
+  else:
+    print('Misinput. Try again!')
+  userChoice = input("Password Manager Menu\n1 - View passwords\n2 - Add new password\n3 - Generate new password\n4 - Re-enter master password (if you made a misinput)\nAny other key - End program\nYour input: ")
+
+print('Thank you for using Password Manager. Good bye!')
