06747f3c42b609faa89d73646b3e7bb70328a0f6
Camdyn Hamilton <c02hamil@gmail.com>
2024-05-11 13:55:27 -0400

    function for encrypting password

--- main.py
+++ main.py
@@ -33,3 +33,14 @@ def _derive_key(password: bytes, salt: bytes, iterations: int = iterations) -> b
     algorithm=hashes.SHA256(), length=32, salt=salt,
     iterations=iterations, backend=backend)
   return b64e(kdf.derive(password))
+
+def password_encrypt(message: bytes, password: str, iterations: int = iterations) -> bytes:
+  salt = secrets.token_bytes(16)
+  key = _derive_key(password.encode(), salt, iterations)
+  return b64(
+    b'%b%b%b' % (
+      salt,
+      iterations.to_bytes(4, 'big'),
+      b64d(Fernet(key).encrypt(message)),
+    )
+  )
