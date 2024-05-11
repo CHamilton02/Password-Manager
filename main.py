e957acfc3a0fde1c6207510539a1c37d5197169f
Camdyn Hamilton <c02hamil@gmail.com>
2024-05-11 13:49:00 -0400

    Add imports for encode/decode funtions

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import secrets
+from base64 import urlsafe_b64encode as b64e, urlsafe_b64decode as b64d
+
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from crpytography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
 password = input("Please enter your master password: ")
 userChoice = input("Password Manager Menu\n1 - View passwords\n2 - Add new password\n3 - Generate new password\n4 - Re-enter master password (if you made a misinput)\nQ - End program\nYour input: ")
 
@@ -15,3 +23,4 @@ while userChoice.lower() != 'q':
   userChoice = input("Password Manager Menu\n1 - View passwords\n2 - Add new password\n3 - Generate new password\n4 - Re-enter master password (if you made a misinput)\nAny other key - End program\nYour input: ")
 
 print('Thank you for using Password Manager. Good bye!')
+
