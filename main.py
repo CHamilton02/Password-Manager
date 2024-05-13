6fb625c325414eb33301b9ee720539bdf8638188
Camdyn Hamilton <c02hamil@gmail.com>
2024-05-13 12:09:48 -0400

    Added generate new password and change master password functions.

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import os
 import secrets
 from base64 import urlsafe_b64encode as b64e, urlsafe_b64decode as b64d
 
-from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet, InvalidToken
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+import random
+
+import string
 
 backend = default_backend()
 iterations = 100_000
@@ -30,11 +34,16 @@ def password_encrypt(message: bytes, password: str, iterations: int = iterations
   )
 
 def password_decrypt(token: bytes, password: str) -> bytes:
-  decoded = b64d(token)
-  salt, iter, token = decoded[:16], decoded[16:20], b64e(decoded[20:])
-  iterations = int.from_bytes(iter, 'big')
-  key = _derive_key(password.encode(), salt, iterations)
-  return Fernet(key).decrypt(token)
+  try:
+    decoded = b64d(token)
+    salt, iter, token = decoded[:16], decoded[16:20], b64e(decoded[20:])
+    iterations = int.from_bytes(iter, 'big')
+    key = _derive_key(password.encode(), salt, iterations)
+    return Fernet(key).decrypt(token)
+  except InvalidToken:
+    print("Incorrect master password. Could not provide user passwords.")
+    return b''
+
 
 def viewPasswords(masterPass: str):
   passwords = open("Passwords.txt", "r")
@@ -56,6 +65,40 @@ def addPassword(masterPass: str):
   passwords.write(newString)
   passwords.close()
 
+def createPassword(chars: list, passSize: int) -> str:
+  newPass = ""
+  for i in range(passSize):
+    value = random.randint(0, len(chars) - 1)
+    newPass += chars[value]
+  return newPass
+
+def generatePassword(masterPass: str):
+  website = input("Please input the website that the password belongs to: ")
+  passType = input("What type of password do you want?\n1 - Only upper and lower case letters (NOT RECOMMENDED)\n2 - Alphanumeric characters (Aa1)\n3 - Alphanumeric + special characters (Aa1^)\nYour input: ")
+  passSize = int(input("How big do you want your password to be? Choice: "))
+  if passType == "1":
+    possChars = list(string.ascii_letters)
+  elif passType == "2":
+    possChars = list(string.ascii_letters + string.digits)
+  elif passType == "3":
+    specChars = "!@#$%^&*()_+-=[]{}|;:',.<>?/~`"
+    possChars = list(string.ascii_letters + string.digits + specChars)
+  newPassword = createPassword(possChars, passSize)
+  encryptedPassword = password_encrypt(newPassword.encode(), masterPass)
+  if os.stat("Passwords.txt").st_size == 0:
+    newString = f'{website}:{encryptedPassword.decode()}'
+  else:
+    newString = f'\n{website}:{encryptedPassword.decode()}'
+  passwords = open("Passwords.txt", "a")
+  passwords.write(newString)
+  passwords.close()
+  print(f"New password added: {website}: {newPassword}")
+
+
+def updateMasterPassword() -> str:
+  updatedPass = input("Please input your actual master password: ")
+  return updatedPass
+
 
 masterPass = input("Please enter your master password: ")
 userChoice = input("Password Manager Menu\n1 - View passwords\n2 - Add new password\n3 - Generate new password\n4 - Re-enter master password (if you made a misinput)\nQ - End program\nYour input: ")
@@ -66,9 +109,9 @@ while userChoice.lower() != 'q':
   elif userChoice == '2':
     addPassword(masterPass)
   elif userChoice == '3':
-    print('12gdrg54536435')
+    generatePassword(masterPass)
   elif userChoice == '4':
-    print('You misclicked lollll')
+    masterPass = updateMasterPassword()
   else:
     print('Misinput. Try again!')
   userChoice = input("\nPassword Manager Menu\n1 - View passwords\n2 - Add new password\n3 - Generate new password\n4 - Re-enter master password (if you made a misinput)\nAny other key - End program\nYour input: ")
